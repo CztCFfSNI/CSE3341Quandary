65b99d6c05cf95ab198753813ec7d4bcf305fe58
CztCFfSNI <jerrychoiczt@gmail.com>
2022-11-16 01:51:01 -0500

    p4 done!

--- Quandary-Public/skeleton/interpreter/Interpreter.java
+++ Quandary-Public/skeleton/interpreter/Interpreter.java
@@ -306,9 +306,8 @@ public class Interpreter {
 
     QInt isNil (Expr e, FunctionDefinition f) {
         QVal x = evaluate(e, f);
-        if (x instanceof QInt) {
-            return new QInt(0);
-        } else if (x instanceof QRef) {
+        if (x instanceof QInt) return new QInt(0);
+        else if (x instanceof QRef) {
             if (((QRef)x).referent == null) return new QInt(1);
             else return new QInt(0);
         } else throw new RuntimeException("isNil Exception!");
@@ -316,12 +315,9 @@ public class Interpreter {
 
     QInt isAtom (Expr e, FunctionDefinition f) {
         QVal x = evaluate(e, f);
-        if (x instanceof QInt) {
-            return new QInt(1);
-        } else if (x instanceof QRef) {
-            if (((QRef)x).referent == null) return new QInt(1);
-            else return new QInt(0);
-        } else throw new RuntimeException("isNil Exception!");
+        if (x instanceof QInt) return new QInt(1);
+        else if (x instanceof QRef) return isNil(e, f);
+        else throw new RuntimeException("isNil Exception!");
     }
 
     QInt setLeft (Expr e1, Expr e2, FunctionDefinition f) {
